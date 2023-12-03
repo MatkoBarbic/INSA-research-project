430c3446f6646979dc927b2a21299666ecbc03a6
Matko <matko.barbic@deegloo.com>
2023-12-04 00:16:20 +0100

    Added Gram-Schmidt pan-scharpening. PSF is now applied in pansharpening_pipeline. Added image tiling.

---
+++ pansharpen_utils.py
@@ -0,0 +1,163 @@
+import numpy as np
+import cv2
+from utils import downsample, apply_psf
+
+def brovy(pan_img: np.array, rgb_img: np.array, weights: list =[0.33, 0.33, 0.33]):
+    '''
+        A function that performs brovy pansharpening.
+
+        Args:
+            pan_img (np.array[int]): High resolution panchromatic (grayscale) image.
+            rgb_img (np.array[int]): Low resolution multispectral (RGB) image.
+            weights (list[int]): Weights for the red, green, and blue channels respectively.
+
+        Returns:
+            (np.array[int]): Pansharpened image.
+    '''
+    
+    dnf = pan_img / np.sum(rgb_img * weights, axis=2)
+    dnf = dnf[:, :, np.newaxis]
+
+    sharp_img = rgb_img * dnf
+    
+    return sharp_img
+
+
+def ihs(pan_img: np.array, rgb_img: np.array):
+    '''
+        A function that performs IHS pansharpening.
+
+        Args:
+            pan_img (np.array[int]): High resolution panchromatic (grayscale) image.
+            rgb_img (np.array[int]): Low resolution multispectral (RGB) image.
+
+        Returns:
+            (np.array[int]): Pansharpened image.
+    '''
+    hls_image = cv2.cvtColor(rgb_img, cv2.COLOR_BGR2HLS)
+    hls_image[:, :, 1] = pan_img
+    sharp_img = cv2.cvtColor(hls_image, cv2.COLOR_HLS2BGR)
+    
+    return sharp_img
+
+
+def projection(v: np.array, u: np.array):
+    '''
+        A function that calculates the projection of a vector v on another vector u.
+
+        Args:
+            v (np.array[float]): Vector to be projected.
+            u (np.array[float]): Vector on which the vector v will be projected.
+
+        Returns:
+            (np.array[float]): Projected vector.
+    '''
+    return ((v @ u) / (u @ u)) * u
+
+
+def gram_schmidt_orth(a: np.array):
+    '''
+        A function that performs brovy pansharpening.
+
+        Args:
+            a (np.array[float]): Multidimensional array to be orthogonalized.
+
+        Returns:
+            (np.array[float]): Orthogonalized array.
+    '''
+    rows, columns = a.shape
+
+    orthogonal_basis = []
+    
+    for c in range(columns):
+        new_base = a[:, c]
+        for base in orthogonal_basis:
+            new_base -= projection(v=new_base, u=base)
+        orthogonal_basis.append(new_base)
+
+    orthogonal_basis = np.array(orthogonal_basis)
+    norm_orth_basis = orthogonal_basis / np.linalg.norm(orthogonal_basis, axis=1, keepdims=True)
+    
+    return norm_orth_basis.T
+
+
+def gram_schmidt_pan(pan_img: np.array, rgb_img: np.array, weights: list =[0.33, 0.33, 0.33]):
+    '''
+        A function that performs Gram-Schmidt pansharpening.
+
+        This algorithm takes in vectors (for example, three vectors in 3D space) that are not orthogonal, and then rotates them so that they are orthogonal afterward. In the case of images, each band (panchromatic, red, green, blue, and infrared) corresponds to one high-dimensional vector (number of dimensions equals number of pixels)
+        
+        Steps:
+            1. Create a low-resolution pan band by computing a weighted average of the multi-spectral bands
+
+            2. These bands are decorrelated using the Gram-Schmidt orthogonalization algorithm, treating each band as one multidimensional vector with simulated low-resolution pan band is used as the first vector
+            
+            3. Low-resolution pan band is then replaced by the high-resolution pan band
+            
+            4. All bands are back-transformed in high resolution
+
+        Some suggested weights for common sensors are as follows (red, green, blue, and infrared, respectively):
+            GeoEye—0.6, 0.85, 0.75, 0.3
+            IKONOS—0.85, 0.65, 0.35, 0.9
+            QuickBird—0.85, 0.7, 0.35, 1.0
+            WorldView-2—0.95, 0.7, 0.5, 1.0
+
+        Args:
+            pan_img (np.array[int]): High resolution panchromatic (grayscale) image.
+            rgb_img (np.array[int]): Low resolution multispectral (RGB) image.
+            weights (list[int]): Weights for the red, green, and blue channels respectively.
+
+        Returns:
+            (np.array[int]): Pansharpened image.
+    '''
+    original_basis = np.identity(3)
+    original_basis[:, 0] = np.array(weights)
+
+    orth_basis = gram_schmidt_orth(a=original_basis)
+
+    # # Uncomment to check if the basis is orthogonal
+    # # product = np.dot(orth_basis, orth_basis.T)
+    # # identity_matrix = np.eye(orth_basis.shape[0])
+
+    # # print(np.allclose(product, identity_matrix))
+
+    orth_bands = rgb_img @ orth_basis
+    orth_bands[:, :, 0] = pan_img
+
+    reverse_coefs = np.linalg.inv(orth_basis)
+    
+    sharp_img = orth_bands @ reverse_coefs
+
+    return sharp_img
+
+
+def pansharp_pipeline(img: np.array, psf: np.array, downsampling_factor: int, algorithm: callable):
+    '''
+        A function that creates a pansharpening pipeline.
+        The pipeline consists of:
+            1. Creating panchromatic (grayscale) version of the image.
+            2. Downsampling the panchromatic and the rgb images.
+            3. Performing a pansharpening algorithm on the images.
+
+        Args:
+            img (np.array[int]): Image on which the pipeline will be performed.
+            psf (np.array[int]): PSF array to be applied to the original image.
+            downsampling_factor (int): Factor by which the images is to be downsampled.
+            algorithm (callable): A pansharpening algorithm which will be performed on the images.
+
+        Returns:
+            pan_img (np.array[int]): Downscaled panchromatic image.
+            rgb_img (np.array[int]): Downscaled RGB image
+            sharp_img (np.array[int]): Pansharpened image.
+    '''
+    img = apply_psf(img=img, psf=psf)
+    
+    pan_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    pan_img = downsample(img=pan_img, factor=downsampling_factor)
+
+    rgb_img = downsample(img=img, factor=downsampling_factor * 2) ## TODO: Find the exact formula to calculate RGB downsampling factor from grayscale downsampling factor
+    rgb_img = cv2.resize(rgb_img, (pan_img.shape))
+
+    sharp_img = algorithm(pan_img=pan_img, rgb_img=rgb_img)
+
+    return pan_img, rgb_img, sharp_img
--- utils.py
+++ utils.py
@@ -2,6 +2,11 @@ import numpy as np
 import math
 import plotly.graph_objects as go
 import plotly.express as px
+import os
+import matplotlib.pyplot as plt
+import rasterio
+from rasterio.enums import Resampling
+
 
 def normal_psf(input_coordinates: list, sigma_x: float, sigma_y: float, eta: float, mean_x: float = 0, mean_y: float = 0):
     '''
@@ -27,6 +32,7 @@ def normal_psf(input_coordinates: list, sigma_x: float, sigma_y: float, eta: flo
 
     psf = 1/(2 * math.pi * math.sqrt(sigma_x*sigma_y)) * float(np.exp(-1/2 * (input_coordinates - mean).T @ inv_cov @ (input_coordinates - mean)))
     return psf
+
 
 def get_normal_psf_for_grid(grid: np.array, sigma_x: float, sigma_y: float, eta: float, mean_x: float = 0, mean_y: float = 0):
     '''
@@ -56,28 +62,134 @@ def get_normal_psf_for_grid(grid: np.array, sigma_x: float, sigma_y: float, eta:
     return psf
 
 
-def update_psf(plot, grid: np.array, sigma_x: float, sigma_y: float, eta: float, mean_x: float, mean_y: float):
+def create_and_save_psf(save_dir_path: str, grid: np.array, sigma_x: float, sigma_y: float, eta: float, mean_x: float = 0, mean_y: float = 0):
     '''
-        Updates the PSF plot.
+        Calculates normal point spread function for a 2D grid and save it to a desired directory.
+        The PSF is saved in the format: {sigma_x}_{sigma_y}_{eta}.npy.
 
         Args:
-            plot: Plot to be updated.
+            save_dir_path (str): Path to the directory in which the PSFs should be saved.
             grid (np.array[int]): The grid containing coordinate values for which the psf value should be calculated.
             sigma_x (float): Variation of the x axis of the 2D normal distribution.
             sigma_y (float): Variation of the y axis of the 2D normal distribution.
             eta (float): Rotation angle.
-            mean_x (float): Mean of the x axis of the 2D normal distribution.
-            mean_y (float): Mean of the y axis of the 2D normal distribution.
+            mean_x (float): Mean of the x axis of the 2D normal distribution. Defaults to 0.
+            mean_y (float): Mean of the y axis of the 2D normal distribution. Defaults to 0.
 
         Returns:
-            (): Updated plot.
+            None
     '''
-    new_psf = get_normal_psf_for_grid(grid=grid, sigma_x=sigma_x, sigma_y=sigma_y, eta=eta, mean_x=mean_x, mean_y=mean_y)
-    new_psf = px.imshow(new_psf, color_continuous_scale='gray')
-    new_psf.update_layout(coloraxis_showscale=False)
+    if not os.path.isdir(save_dir_path):
+        os.makedirs(save_dir_path)
+              
+    psf = get_normal_psf_for_grid(grid=grid, sigma_x=sigma_x, sigma_y=sigma_y, eta=eta, mean_x=mean_x, mean_y=mean_y)
+    np.save(os.path.join(save_dir_path, "{sigma_x}_{sigma_y}_{eta}.npy".format(sigma_x=sigma_x, sigma_y=sigma_y, eta=eta)), psf)
 
-    new_psf = go.FigureWidget(new_psf)
-    plot.data[0].z = new_psf.data[0].z
 
-
+def load_psf(save_dir_path: str, sigma_x: float, sigma_y: float, eta: float):
+    '''
+        Loads a saved PSF based off of sigma_x, sigma_y and eta parameters.    
     
+        Args:
+            save_dir_path (str): Path to the directory in which the PSFs should be saved.
+            sigma_x (float): Variation of the x axis of the 2D normal distribution.
+            sigma_y (float): Variation of the y axis of the 2D normal distribution.
+            eta (float): Rotation angle.
+
+        Returns:
+            (np.array[int]): Loaded PSF.
+    '''
+    psf = np.load(os.path.join(save_dir_path, "{sigma_x}_{sigma_y}_{eta}.npy".format(sigma_x=sigma_x, sigma_y=sigma_y, eta=eta)))
+    return psf
+
+
+def apply_psf(img: np.array, psf: np.array):
+    '''
+        Apply PSF to image.    
+    
+        Args:
+            img (np.array[int]): The image to which the PSF should be applied.
+            psf (np.array[int]): The point spread function.
+
+        Returns:
+            (np.array[int]): The imaged after appling the PSF.
+    '''
+    convolved_channels = []
+    fft_psf = np.fft.fft2(psf)
+
+    for channel in range(img.shape[-1]):
+        fft_img = np.fft.fft2(img[:, :, channel])
+        psf_img = np.abs(np.fft.ifftshift(np.fft.ifft2(fft_img * fft_psf)))
+        convolved_channels.append(psf_img)
+
+    psf_img = np.stack(convolved_channels, axis=-1)
+    return psf_img.astype(np.uint8)
+
+
+def downsample(img: np.array, factor: int):
+    '''
+        Downscample an image by a factor.
+
+        Args:
+            grid (np.array[int]): Image to be downscaled.
+            factor (int): Factor by which the images is to be downsampled.
+
+        Returns:
+            (np.array[int]): Downsampled image.
+    '''
+    return img[::factor, ::factor]
+
+
+def create_tiles(path_to_image: str, output_folder: str, tile_size: int, overlap: int = 0, save_metadata: bool = False):
+    '''
+    Create tiles from a large raster image.
+
+    Args:
+        path_to_image (str): Path to the input raster image.
+        output_folder (str): Path to the output folder where tiles will be saved.
+        tile_size (int): Size of each tile.
+        overlap (int): Number of pixels to overlap between adjacent tiles.
+        save_metadata (bool): Whether metadata should be saved along with the images. (TODO)
+
+    Returns:
+        None
+    '''
+    # Open the input raster image using rasterio
+    with rasterio.open(path_to_image) as src:
+        # Calculate the number of rows and columns for tiles
+        num_rows = (src.height - overlap) // (tile_size - overlap)
+        num_cols = (src.width - overlap) // (tile_size - overlap)
+
+        # Iterate over rows and columns to create tiles
+        for row in range(num_rows):
+            for col in range(num_cols):
+                # Define a window for each tile
+                window = rasterio.windows.Window(
+                    col * (tile_size - overlap),
+                    row * (tile_size - overlap),
+                    tile_size, tile_size
+                )
+
+                # Read the data for the tile
+                tile_data = src.read(window=window)
+
+                # Create a new dataset profile for the tile
+                tile_profile = src.profile.copy()
+                tile_profile.update({
+                    'width': tile_size,
+                    'height': tile_size,
+                    'transform': src.window_transform(window),
+                    'driver': 'JPEG',  # Specify the driver for JPEG format
+                    'count': 3 if len(src.shape) == 3 else 1,  # 3 bands for RGB, adjust as needed
+                    'dtype': 'uint8',  # Use uint8 for JPG images
+                })
+
+                if not os.path.isdir(output_folder):
+                    os.makedirs(output_folder)
+
+                # Define the output path for the tile
+                output_path = os.path.join(output_folder, f'tile_{row}_{col}.jpg')
+
+                # Write the tile data to a new JPEG file
+                with rasterio.open(output_path, 'w', **tile_profile) as dst:
+                    dst.write(tile_data.astype('uint8'))  # Ensure data type is uint8 for JPEG
