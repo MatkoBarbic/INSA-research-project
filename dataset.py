d4a14e2fa0cad71dd3b9ca1597c3d5f10caf5955
Barbic Matko <barbic@insa-10825>
2023-12-20 15:38:05 +0100

    Dataset class now generates pan and rgb image online. Trained model on SOTA dataset and visualised the results in results.ipynb

--- dataset.py
+++ dataset.py
@@ -2,31 +2,49 @@ import torch
 import torchvision
 from PIL import Image
 import os
+from utils import downsample
+import numpy as np
 
 class PanDataset(torch.utils.data.Dataset):
-    def __init__(self, path_to_pan, path_to_rgb, path_to_sharp):
+    def __init__(self, path_to_pan, path_to_rgb, path_to_sharp, train_perc, train: bool):
         self.path_to_pan = path_to_pan
         self.path_to_rgb = path_to_rgb
         self.path_to_sharp = path_to_sharp
-        
-        self.pan = sorted(os.listdir(self.path_to_pan))
-        self.rgb = sorted(os.listdir(self.path_to_rgb))
-        self.sharp = sorted(os.listdir(self.path_to_sharp))
+
+        self.train = train
+
+        if train:
+          self.train_split = round(len(os.listdir(self.path_to_pan)) * train_perc)
+
+          self.pan = sorted(os.listdir(self.path_to_pan))[: self.train_split]
+          self.rgb = sorted(os.listdir(self.path_to_rgb))[: self.train_split]
+          self.sharp = sorted(os.listdir(self.path_to_sharp))[: self.train_split]
+
+        else:
+          self.pan = sorted(os.listdir(self.path_to_pan))[self.train_split: ]
+          self.rgb = sorted(os.listdir(self.path_to_rgb))[self.train_split: ]
+          self.sharp = sorted(os.listdir(self.path_to_sharp))[self.train_split: ]
 
         assert len(self.pan) == len(self.rgb) == len(self.sharp)
 
     def __len__(self):
-        return len(os.listdir(self.path_to_sharp))
+        if self.train:
+          return self.train_split
+        else:
+          return len(os.listdir(self.path_to_sharp)) - self.train_split
 
     def __getitem__(self, index):
-        pan_img = torchvision.transforms.functional.to_tensor(Image.open(os.path.join(self.path_to_pan, self.pan[index])))
-        rgb_img = torchvision.transforms.functional.to_tensor(Image.open(os.path.join(self.path_to_rgb, self.pan[index])))
-        sharp_img = torchvision.transforms.functional.to_tensor(Image.open(os.path.join(self.path_to_sharp, self.pan[index])))
+        # pan_img = torchvision.transforms.functional.to_tensor(Image.open(os.path.join(self.path_to_pan, self.pan[index])))
+        # rgb_img = torchvision.transforms.functional.to_tensor(Image.open(os.path.join(self.path_to_rgb, self.pan[index])))
+        # sharp_img = torchvision.transforms.functional.to_tensor(Image.open(os.path.join(self.path_to_sharp, self.pan[index])))
 
-        if pan_img.shape != (1, 410, 410):
-            print(self.path_to_pan)
-        
-        if rgb_img.shape != (3, 410, 410):
-            print(self.path_to_rgb)
+        sharp_img = Image.open(os.path.join(self.path_to_sharp, self.sharp[index]))
+        pan_img = sharp_img.convert("L")
+        rgb_img = downsample(np.array(sharp_img), 5)
+        rgb_img = Image.fromarray(rgb_img).resize(sharp_img.size)
+
+        sharp_img = torchvision.transforms.functional.to_tensor(sharp_img)
+        pan_img = torchvision.transforms.functional.to_tensor(pan_img)
+        rgb_img = torchvision.transforms.functional.to_tensor(rgb_img)
 
         return (torch.cat([pan_img, rgb_img], dim=0), sharp_img)
--- unet/train.py
+++ unet/train.py
@@ -17,6 +17,23 @@ from dataset import PanDataset
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+
+def evaluate(net, val_loader, criterion):
+	net.eval()
+	val_loss = 0.0
+	
+	with torch.no_grad():
+		for x_val, y_val in val_loader:
+			x_val, y_val = x_val.to(device), y_val.to(device)
+			outputs_val = net(x_val)
+			loss_val = criterion(outputs_val, y_val)
+			val_loss += loss_val.item()
+	
+	val_loss /= len(val_loader)
+	
+	return val_loss
+
+
 def train(net, optimizer, train_loader, val_loader, model_path, epochs=5, writer=None):
     criterion = torch.nn.MSELoss()
     best_val_loss = float('inf')
@@ -34,15 +51,7 @@ def train(net, optimizer, train_loader, val_loader, model_path, epochs=5, writer
             optimizer.step()
             t.set_description(f'training loss: {np.mean(train_loss)}')
 
-        net.eval()
-        val_loss = 0.0
-        with torch.no_grad():
-            for x_val, y_val in val_loader:
-                x_val, y_val = x_val.to(device), y_val.to(device)
-                outputs_val = net(x_val)
-                loss_val = criterion(outputs_val, y_val)
-                val_loss += loss_val.item()
-        val_loss /= len(val_loader)
+        val_loss = evaluate(net=unet, val_loader=val_loader, criterion=criterion)
         print(f'validation loss: {val_loss}')
 
         if val_loss < best_val_loss:
@@ -62,66 +71,68 @@ def train(net, optimizer, train_loader, val_loader, model_path, epochs=5, writer
 
 
 if __name__=='__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--data_path', type=str, default = '../data/landscapes', help='dataset path')
-    parser.add_argument('--batch_size', type=int, default = int(32), help='batch_size')
-    parser.add_argument('--lr', type=float, default = float(1e-3), help='learning rate')
-    parser.add_argument('--epochs', type=int, default = int(10), help='number of epochs')
-    parser.add_argument('--num_workers', type=int, default = int(4), help='number of workers')
- 
-    args = parser.parse_args()
-    data_path = args.data_path
-    batch_size = args.batch_size
-    lr = args.lr
-    epochs = args.epochs
-    num_workers = args.num_workers
+	torch.cuda.empty_cache()
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--data_path', type=str, default = '../data/landscapes', help='dataset path')
+	parser.add_argument('--batch_size', type=int, default = int(4), help='batch_size')
+	parser.add_argument('--lr', type=float, default = float(1e-3), help='learning rate')
+	parser.add_argument('--epochs', type=int, default = int(10), help='number of epochs')
+	parser.add_argument('--num_workers', type=int, default = int(4), help='number of workers')
 
-    model_folder = "./models/unet/"
+	args = parser.parse_args()
+	data_path = args.data_path
+	batch_size = args.batch_size
+	lr = args.lr
+	epochs = args.epochs
+	num_workers = args.num_workers
+	
+	train_perc = 0.8
 
-    if not os.path.isdir(model_folder):
-        os.makedirs(model_folder)
-    
-    model_path = os.path.join(model_folder, "unet.pth")
-    
-    path_to_data = "./data/"
-    path_to_train = os.path.join(path_to_data, "train")
-    path_to_val = os.path.join(path_to_data, "val")
+	model_folder = "./models/unet/"
 
-    path_to_train_pan = os.path.join(path_to_train, "pan")
-    path_to_train_rgb = os.path.join(path_to_train, "rgb")
-    path_to_train_sharp = os.path.join(path_to_train, "sharp")
+	if not os.path.isdir(model_folder):
+		os.makedirs(model_folder)
 
-    path_to_val_pan = os.path.join(path_to_val, "pan")
-    path_to_val_rgb = os.path.join(path_to_val, "rgb")
-    path_to_val_sharp = os.path.join(path_to_val, "sharp")
+	model_path = os.path.join(model_folder, "unet.pth")
 
-    unet = UNet(n_channels=4, n_classes=3)# .cuda()
-    train_data = PanDataset(path_to_pan=path_to_train_pan, path_to_rgb=path_to_train_rgb, path_to_sharp=path_to_train_sharp)
-    train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=num_workers)
-    
-    val_data = PanDataset(path_to_pan=path_to_val_pan, path_to_rgb=path_to_val_rgb, path_to_sharp=path_to_val_sharp)
-    val_loader = DataLoader(val_data, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+	path_to_data = "./data/"
+	path_to_data = path_to_data + "SOTA/"
 
-    optimizer = optim.Adam(unet.parameters(), lr=lr)
-    writer = SummaryWriter('./runs/UNet')
-    train_loss, val_loss = train(net=unet, optimizer=optimizer, train_loader=train_loader, val_loader=val_loader, model_path=model_path, epochs=epochs, writer=writer)
- 
-    # x, y = next(iter(train_loader))
- 
-    # with torch.no_grad():
-    #     all_embeddings = []
-    #     all_labels = []
-    #     for x, y in train_loader:
-    #         x , y = x.to(device), y.to(device)
-    #         embeddings = unet.get_features(x).view(-1, 128*28*28)
-    #         all_embeddings.append(embeddings)
-    #         all_labels.append(y)
-    #         if len(all_embeddings)>6:
-    #             break
-    #     embeddings = torch.cat(all_embeddings)
-    #     labels = torch.cat(all_labels)
-    #     writer.add_embedding(embeddings, label_img=labels, global_step=1)
-    #     writer.add_graph(unet, x.to(device))
-        
-    # Save model weights
-    # torch.save(unet.state_dict(), 'unet.pth')
+	if "SOTA" not in path_to_data:
+		path_to_train = os.path.join(path_to_data, "train")
+		path_to_val = os.path.join(path_to_data, "val")
+
+		path_to_train_pan = os.path.join(path_to_train, "pan")
+		path_to_train_rgb = os.path.join(path_to_train, "rgb")
+		path_to_train_sharp = os.path.join(path_to_train, "sharp")
+
+		path_to_val_pan = os.path.join(path_to_val, "pan")
+		path_to_val_rgb = os.path.join(path_to_val, "rgb")
+		path_to_val_sharp = os.path.join(path_to_val, "sharp")
+
+	else:
+		path_to_train_pan = os.path.join(path_to_data, "pan")
+		path_to_train_rgb = os.path.join(path_to_data, "rgb")
+		path_to_train_sharp = os.path.join(path_to_data, "sharp")
+
+		path_to_val_pan = os.path.join(path_to_data, "pan")
+		path_to_val_rgb = os.path.join(path_to_data, "rgb")
+		path_to_val_sharp = os.path.join(path_to_data, "sharp")
+
+	unet = UNet(n_channels=4, n_classes=3).cuda()
+	# train_data = PanDataset(path_to_pan=path_to_train_pan, path_to_rgb=path_to_train_rgb, path_to_sharp=path_to_train_sharp)
+	# train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+
+	# val_data = PanDataset(path_to_pan=path_to_val_pan, path_to_rgb=path_to_val_rgb, path_to_sharp=path_to_val_sharp)
+	# val_loader = DataLoader(val_data, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+
+	train_data = PanDataset(path_to_pan=path_to_train_pan, path_to_rgb=path_to_train_rgb, path_to_sharp=path_to_train_sharp, train_perc=train_perc, train=True)
+	train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+
+	val_data = PanDataset(path_to_pan=path_to_val_pan, path_to_rgb=path_to_val_rgb, path_to_sharp=path_to_val_sharp, train_perc=1 - train_perc, train=True)
+	val_loader = DataLoader(val_data, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+
+	optimizer = optim.Adam(unet.parameters(), lr=lr)
+	writer = SummaryWriter('./runs/UNet')
+	print(torch.cuda.device_count())
+	train_loss, val_loss = train(net=unet, optimizer=optimizer, train_loader=train_loader, val_loader=val_loader, model_path=model_path, epochs=epochs, writer=writer)
